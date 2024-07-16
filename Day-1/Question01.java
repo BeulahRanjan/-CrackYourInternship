517fda07ed94439a8d30af3069537c2f3a49369e
Beulah Ranjan <beulahranjan@gmail.com>
2024-07-16 16:32:04 +0530

    Updates

---
+++ Day-1/Question01.java
@@ -0,0 +1,28 @@
+// Question 04-Reverse Linked List
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+public class Question01 {
+    public ListNode reverseList(ListNode head) 
+    {ListNode prev = null;  
+        ListNode current = head;
+    
+        
+        while(current != null) { 
+            ListNode next = current.next; 
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+       return prev;
+    }
+}
